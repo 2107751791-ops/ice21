6e0372b9f16ee4628251eb9f1ddf4453aedaaa63
2107751791-ops <2107751791@qq.com>
2026-08-20 01:23:30 +0800

    Delete 双游戏联合正式版-无编辑工具-2026-08-19 directory

--- 双游戏联合正式版-无编辑工具-2026-08-19/打开双游戏.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-set "NODE_EXE=C:\Users\86158\.cache\codex-runtimes\codex-primary-runtime\dependencies\node\bin\node.exe"
-if not exist "%NODE_EXE%" set "NODE_EXE=node"
-start "Mammoth Two Games Server" /min cmd /c ""%NODE_EXE%" server.mjs"
-timeout /t 2 /nobreak >nul
-start "" "http://127.0.0.1:4211/"
